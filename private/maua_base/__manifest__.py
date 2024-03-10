198ba8163e967b6621917a0eaf8b088ebe8b5ace
Manoel Roberto <manoelrobertoms@gmail.com>
2024-03-10 20:46:12 -0300

    [NEW] criação  maua_base e configuração do odoo.conf private.

---
+++ private/maua_base/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Manoel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Maua Base',
+    'description': """
+        Maua Base Addons""",
+    'version': '16.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Manoel',
+    'website': 'https://github.com/manoel-roberto',
+    'depends': [
+	'contacts',
+	'web_responsive',
+    ],
+}
